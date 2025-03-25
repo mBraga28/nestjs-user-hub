8455e71637978901e543459cddd20b4076cfe14d
Marco Braga <maboa17@gmail.com>
2025-03-25 13:11:07 -0300

    fix cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://react-user-hub.vercel.app',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('User Service API')
